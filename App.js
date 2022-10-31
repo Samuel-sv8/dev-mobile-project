568ce146f26aa35062364834b387f41564357404
unknown <samuelsm360@gmail.com>
2022-10-31 14:33:48 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,32 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+import { 
+  StyleSheet,
+  Text,
+  View, 
+  Image, 
+  SafeAreaView,
+  Button,
+  Pressable} from 'react-native';
 import bolo from './assets/bolo.PNG';
+import bolomilho from './assets/bolomilho.png';
+import bolofofo from './assets/bolofofo.png';
+import bolomorango from './assets/bolomorango.png';
 
 export default function App() {
+  
   return (
+    <SafeAreaView>
     <View style={styles.container}>
       <Text style = {styles.firstTexto}> Casa do Bolo</Text>
-      <Image source = {bolo} style = {styles.foto}/>
-      
+      <Text style = {styles.twoTexto}> Escolha a opcao desejada </Text>
+      <Text style = {styles.threTexto}> 1- Chocolate </Text>  
+      <Image source = {bolo} style = {styles.foto}/>      
+      <Text style = {styles.fourTexto}> 2- Milho </Text>
+      <Image source = {bolomilho} style = {styles.fototwo}/>  
+      <Text style = {styles.fiveTexto}> 3- Fofo </Text>
+      <Image source = {bolofofo} style = {styles.fotothre}/>  
+      <Text style = {styles.sixTexto}> 4- Morango </Text>
+      <Image source = {bolomorango} style = {styles.fotofour}/> 
+
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+    alignItems: 'side',
+    justifyContent: 'side',
+    
   },
   firstTexto : {
     color : 'blue',
     fontSize : 22,
+    height : 50,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+  },
+  twoTexto : {
+    color : 'black',
+    fontSize : 22,
+    height : 40,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+  },
+  threTexto : {
+    color : 'brown',
+    fontSize : 22,
+    height : 40,
+    fontWeight: 'bold',
+  },
+  fourTexto : {
+    color : 'yellow',
+    fontSize : 22,
+    height : 40,
+    fontWeight: 'bold',
+  },
+  fiveTexto : {
+    color : 'malt',
+    fontSize : 22,
+    height : 40,
+    fontWeight: 'bold',
+  },
+  sixTexto : {
+    color : 'red',
+    fontSize : 22,
+    height : 40,
+    fontWeight: 'bold',
   },
   foto :  {
-    width : 300,
-    height : 200,
+    width : 150,
+    height : 150,
+  
+  },
+  fototwo :  {
+    width : 150,
+    height : 150,
+  
+  },
+  fotothre :  {
+    width : 150,
+    height : 150,
+  
+  },
+  fotofour :  {
+    width : 150,
+    height : 150,
   
   }
 });
